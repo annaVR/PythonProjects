7b71b3b48de4c2fe701e5f2bf11710937612917d
annaVR <anna@kirsanova.name>
2017-02-09 20:40:29 -0800

    mac commit

--- Udemy_python_anyone_can_code/working_with_elements/browser_interactions.py
+++ Udemy_python_anyone_can_code/working_with_elements/browser_interactions.py
@@ -6,4 +6,31 @@ class BrowserInteractions(object):
 
     def test(self):
         driver = webdriver.Firefox()
+        #it is better to max window before open the url
+        driver.maximize_window()
         driver.get('https://letskodeit.teachable.com/p/practice')
+
+        print(driver.title)
+
+        print(driver.current_url)
+
+        print('Refresh for the first time:')
+        driver.refresh()
+
+        print('Refresh for the second time:')
+        driver.get(driver.current_url)
+
+        driver.page_source
+
+        driver.get('http://www.thegeekstuff.com/2010/08/bash-shell-builtin-commands/')
+
+        driver.back()
+
+        driver.forward()
+
+        driver.close()
+
+        driver.quit()
+
+ff = BrowserInteractions()
+ff.test()
