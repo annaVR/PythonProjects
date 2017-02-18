4e234b5c7afa4bd24eeba7f7feb10c2b2a5028eb
anna@kirsanova.name <anna@kirsanova.name>
2017-02-17 22:12:23 -0800

    swd studies

--- Udemy_python_anyone_can_code/wait_types/explicit_wait_demo.py
+++ Udemy_python_anyone_can_code/wait_types/explicit_wait_demo.py
@@ -1,1 +1,73 @@
 __author__ = 'anna'
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class ExplictWaitDemo():
+
+    def test(self):
+        url = 'http://www.expedia.com'
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(url)
+        driver.implicitly_wait(4)
+
+        driver.find_element(By.ID, "tab-flight-tab").click()
+        driver.find_element(By.ID, "flight-origin").send_keys("SFO")
+        driver.find_element(By.ID, 'flight-origin').send_keys(Keys.RETURN)
+        driver.find_element(By.ID, "flight-destination").send_keys("NYC")
+        driver.find_element(By.ID, 'flight-destination').send_keys(Keys.RETURN)
+
+        driver.find_element(By.ID, "flight-departing").click()
+        driver.find_element(By.XPATH,
+                            "//caption[contains(text(),'FEB 2017')]/parent::table//button[contains(text(), '25')]").click()
+
+        time.sleep(3)
+        return_date = driver.find_element(By.ID, "flight-returning").click()
+        driver.find_element(By.XPATH,
+                            "//caption[contains(text(),'FEB 2017')]/parent::table//button[contains(text(), '27')]").click()
+        time.sleep(3)
+        #another test for the car tab
+        # driver.find_element(By.ID, "tab-car-tab").click()
+        # driver.find_element(By.ID, "car-pickup").send_keys('San Francisco\n')
+        # time.sleep(2)
+        # driver.find_element(By.ID, 'car-dropoff').send_keys('Los Angeles\n')
+        # time.sleep(2)
+        #
+        # pickup_date = driver.find_element(By.ID, "car-pickup-date")
+        # pickup_date.clear()
+        # pickup_date.send_keys('02/25/2017\n')
+        # time.sleep(2)
+        # pickup_time = driver.find_element(By.ID, "car-pickup-time")
+        # time.sleep(2)
+        #
+        # pickup_time_options = Select(pickup_time)
+        # pickup_time_options.select_by_value('900AM')
+        # driver.find_element(By.ID, "car-dropoff-date").send_keys('02/27/2017\n')
+        # dropoff_time = driver.find_element(By.ID, "car-dropoff-time")
+        # dropoff_time_options = Select(dropoff_time)
+        # dropoff_time_options.select_by_value("600PM")
+        # time.sleep(4)
+        driver.find_element(By.ID, "search-button").click()
+
+        wait = WebDriverWait(driver, 60, poll_frequency=1,
+                             ignored_exceptions=[NoSuchElementException,
+                                                 ElementNotVisibleException,
+                                                 ElementNotSelectableException])
+        element = wait.until(EC.element_to_be_clickable((By.ID, 'stopFilter_stops-0')))
+        time.sleep(2)
+        element.click()
+        time.sleep(2)
+        driver.quit()
+
+ff = ExplictWaitDemo()
+ff.test()
+
--- Udemy_python_anyone_can_code/working_with_elements/dynamyc_xpath_format.py
+++ Udemy_python_anyone_can_code/working_with_elements/dynamyc_xpath_format.py
@@ -2,7 +2,9 @@ __author__ = 'anna'
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+import random
 
 class DynamicXPathFormat():
 
@@ -12,25 +14,22 @@ class DynamicXPathFormat():
         driver = webdriver.Firefox()
         driver.maximize_window()
         driver.get(url)
-        driver.implicitly_wait(4)
+        driver.implicitly_wait(3)
 
         #login
         driver.find_element(By.LINK_TEXT, 'Login').click()
         driver.find_element(By.ID, 'user_email').send_keys('test@email.com')
         driver.find_element(By.ID, 'user_password').send_keys('abcabc')
         driver.find_element(By.NAME, 'commit').click()
-        driver.implicitly_wait(3)
 
-        #select courses one by one
+        #select random course from the list and open its link
         courses_list = driver.find_elements(By.CSS_SELECTOR, '.course-listing-title')
-        for course in courses_list:
-            title = course.text
-            xpath = '//div[contains(@class,"course-listing-title") and contains(text(), "{}")]'.format(title)
-            driver.find_element(By.XPATH, xpath).click()
-            time.sleep(2)
-            driver.back()
-            time.sleep(2)
-            driver.implicitly_wait(3)
+        random_course = random.choice(courses_list)
+        title = random_course.get_attribute('innerText')
+        print(title)
+        xpath = '//div[contains(@class,"course-listing-title") and contains(text(), "{}")]'.format(title)
+        driver.find_element(By.XPATH, xpath).click()
+        time.sleep(2)
         driver.quit()
 ff = DynamicXPathFormat()
 ff.test()
